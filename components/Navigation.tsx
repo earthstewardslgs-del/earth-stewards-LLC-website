9d60cccc39c5e8ff992c6583b0eb242092cac97c
earthstewardslgs-del <earthstewardslgs@gmail.com>
2026-02-09 17:16:14 -0500

    update

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -31,7 +31,7 @@ export default function Navigation() {
           : 'bg-transparent'
       }`}
     >
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-2">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
         <div className="flex justify-between items-center h-20">
           {/* Logo */}
           <Link href="/" className="flex items-center space-x-3 group">

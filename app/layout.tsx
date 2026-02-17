b0f30e12614538bf407630903e9d31061233dcd2
earthstewardslgs-del <earthstewardslgs@gmail.com>
2026-02-17 15:38:03 -0500

    update pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import StructuredData from '@/components/StructuredData'
 import './globals.css'
 
@@ -56,10 +57,45 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1627823055173903');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+      </head>
       <body>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1627823055173903&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <StructuredData />
         {children}
       </body>
     </html>
   )
+}
+
+declare global {
+  interface Window {
+    fbq: (action: string, eventName: string, params?: Record<string, unknown>) => void
+  }
 }
